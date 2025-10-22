11aec883fa9d0289b611dec2f28f301358d07ffe
iamShimanto <shimantowebdeveloper@gmail.com>
2025-10-23 01:30:09 +0600

    seo fix

--- client/next-sitemap.config.js
+++ client/next-sitemap.config.js
@@ -1,19 +1,5 @@
-/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://aliairtravels.com",
+  siteUrl: "https://www.aliairtravels.com",
   generateRobotsTxt: true,
-  sitemapSize: 5000, 
-  changefreq: "daily",
-  priority: 0.8,
-  exclude: ["/admin/*", "/login/*", "/api/*"],
-  alternateRefs: [
-    {
-      href: "https://aliairtravels.com/",
-      hreflang: "en",
-    },
-    {
-      href: "https://aliairtravels.com/",
-      hreflang: "bn",
-    },
-  ],
+  sitemapSize: 7000,
 };
